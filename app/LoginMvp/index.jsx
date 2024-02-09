77ff4970e065a2adfb853e9885d1eb61c32e390e
Gustavo Henrique <gustavohlma8@gmail.com>
2024-02-09 17:51:04 -0300

    fix(feature/container-navbar-descarte): corrigindo erro na hora de chamar imports

---
+++ app/LoginMvp/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+    <Text>
+      Hello, World!
+    </Text>
+    </>
+  )
+}
