29e9a242766822c3658ea573de0a0b01881e7ddc
luislauriano <luislauriano@Outlook.com.br>
2024-02-13 05:09:39 -0300

    Primeira finalização por completa da tela home de descarte

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import LocalizaIcon from './localiza.png'
+
+export {LocalizaIcon}
+
+import ProfilePic from './Ellipse 3.png'
+
+export {ProfilePic}
+
+
+import product1 from './produto1.png'
+
+export {product1}
+
+import product2 from './produto2.png'
+
+export {product2}
+
+import product3 from './produto3.png'
+
+export {product3}
+
+import product4 from './produto4.png'
+
+export {product4}
+
+
+import chatIcon from './chatIcon.png'
+
+export {chatIcon}
